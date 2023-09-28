d86d552404505e834a13577575a25bcb18e1492c
Mariano Passerini <renacer258@gmail.com>
2023-09-28 16:37:49 -0300

    Now it displays an array of objects [Item: [SubItems]]

--- click.js
+++ click.js
@@ -1,52 +1,55 @@
 import puppeteer from 'puppeteer';
 
 (async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
-        defaultViewport: null,
-        userDataDir: './user_data',
-    });
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    userDataDir: './user_data',
+  });
 
-    const page = await browser.newPage();
-    await page.goto('https://compragamer.com/?seccion=3');
+  const page = await browser.newPage();
+  await page.goto('https://compragamer.com/?seccion=3');
 
-    const acordionCategories = await page.$$('.noMobile > cgw-category-list > mat-accordion > mat-expansion-panel > div > div > mat-accordion > mat-expansion-panel');
-    const acordionSubCategories = await page.$$('.noMobile > cgw-category-list > mat-accordion > mat-expansion-panel > div > div > mat-accordion > mat-expansion-panel > div > div > p');
-    console.log(acordionSubCategories.length)
+  const acordionCategories = await page.$$('.noMobile > cgw-category-list > mat-accordion > mat-expansion-panel > div > div > mat-accordion > mat-expansion-panel');
 
-    let subItemsArray = []
+  let categoryData = [];
 
-    for (const accordionCategory of acordionCategories) {
-        try {
-          let item = await page.evaluate(el => el.querySelector("mat-expansion-panel-header > span > mat-panel-title").textContent, accordionCategory);
-          
-          // Click on the header to expand the category
-          await accordionCategory.click(item);
-    
-          // Wait for a specific duration (e.g., 1000 milliseconds or 1 second)
-          const delayTime = 1000;
-          await new Promise(resolve => setTimeout(resolve, delayTime));
-    
-          // Use page.evaluate within the context of accordionCategory to get nested span values
-          let subItems = await page.evaluate(el => {
-            const spans = Array.from(el.querySelectorAll("div > div > p > span"));
-            return spans.map(span => span.textContent);
-          }, accordionCategory);
-    
-          console.log(`Category: ${item} - SubItems: ${subItems}`); 
-        } catch (e) {
-          console.log(e);
-        }
-      }
+  for (const accordionCategory of acordionCategories) {
+    try {
+      let category = await page.evaluate(el => el.querySelector("mat-expansion-panel-header > span > mat-panel-title").textContent, accordionCategory);
 
-    // for(const accordionSubCategory of acordionSubCategories) {
-    //     try {
-    //         let subItem = await page.evaluate(el => el.querySelector("span").textContent, accordionSubCategory);
-    //         subItemsArray.push(subItem);
-    //     } catch (e) {
-    //         console.log(e);
-    //     }
-    //     console.log(subItemsArray);
-    // }
-    // await browser.close();
+      // Remove spaces and hyphens from the category
+      category = category.replace(/[\s-]/g, '');
+
+      // Click on the header to expand the category
+      await accordionCategory.click(category);
+
+      // Wait for a specific duration (e.g., 1000 milliseconds or 1 second)
+      const delayTime = 1000;
+      await new Promise(resolve => setTimeout(resolve, delayTime));
+
+      // Use page.evaluate within the context of accordionCategory to get nested span values
+      let subItems = await page.evaluate(el => {
+        const spans = Array.from(el.querySelectorAll("div > div > p > span"));
+        return spans.map(span => span.textContent);
+      }, accordionCategory);
+
+      // Remove spaces and hyphens from each sub-item
+      subItems = subItems.map(item => item.replace(/[\s-]/g, ''));
+
+      // Create an object with the category as the key and sub-items as the value
+      let categoryObject = {
+        [category]: subItems
+      };
+
+      // Push the categoryObject into the categoryData array
+      categoryData.push(categoryObject);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  console.log(categoryData);
+
+  // await browser.close();
 })();
